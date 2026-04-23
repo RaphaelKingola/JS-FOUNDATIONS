e3a4e97298e9bf2af38679ade89061e465213211
Raphael Muthini <raffrachake@gmail.com>
2026-04-23 18:17:38 +0300

    add Vairables notes fundamentals of js

---
+++ 1-variables.js
@@ -0,0 +1,32 @@
+/* 
+VARIABLES - Refers to storage containers used to store data in a program.
+
+Examples Of variables include:
+1. Numbers (Age=24)
+2. text (Name="John")
+3. Booleans (isStudent=true)
+4. collections[arrays] (items = [1, 2, 3]) etc.
+
+-----CHARACTERISTICS OF GOOD VARIABLES -----
+1. Descriptive and Meaningful; A variable name should clearly communicate what data it holds and why it exists.
+avoid single letters (x, y, z) unless they are used in a very specific context (like loop counters).
+* Poor: d=8600  *Good: Seconds_per_day=86400
+
+2. Pronouncable and searchable: A variable  should be easy to pronounce and search for in the codebase.
+avoid abbreviations and acronyms that may not be universally understood.
+* Poor: usr_Cnt  *Good: User_Count
+
+3. Consistent Naming: A variable name should follow a consistent naming convention throughout the codebase. 
+Stick to one casing style throughout your project
+* Poor: userName, user_name, UserName  *Good: userName (camelCase)
+---------------TYPES OF CASES ---------------
+A. Snake Case: user_registration_date (Common in Python)
+
+B. Camel Case: userRegistrationDate (Common in JavaScript/Java)
+
+C. Pascal Case: UserRegistrationDate (Common for Classes)
+
+4. INTENTION REVEALING:
+
+
+*/
